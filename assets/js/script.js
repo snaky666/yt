d066d173ab4aad3a68286df4af9d99a571d4f79d
snaky666 <madimoh44@gmail.com>
2025-10-03 16:16:38 +0100

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-const sidebar=document.getElementById('sidebar');
-const openBtn=document.getElementById('sidebar-open');
-const closeBtn=document.getElementById('sidebar-close');
-openBtn.addEventListener('click',()=>sidebar.classList.add('open'));
-closeBtn.addEventListener('click',()=>sidebar.classList.remove('open'));
