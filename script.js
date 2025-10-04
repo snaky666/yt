0b4e7fa30ebd112922983e3a0d6841ba556c9b6f
snaky666 <madimoh44@gmail.com>
2025-10-04 02:56:46 +0100

    Add Nova Academy site files.

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuToggle = document.querySelector('.menu-toggle');
+const sidebar = document.querySelector('.sidebar');
+
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('active');
+});
