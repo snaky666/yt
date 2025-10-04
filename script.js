4e30ee3762c4c9c8fa695c0607ca821711549721
snaky666 <madimoh44@gmail.com>
2025-10-04 03:06:51 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const sidebar = document.querySelector('.sidebar');
-
-menuToggle.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
-});
